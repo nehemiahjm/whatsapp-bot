db67f0b1b32012028d838074e8c066ac11866044
Nehemiah Jerald <nehemiahjerald@Nehemiahs-MacBook-Pro.local>
2026-02-21 03:19:29 +0500

    first commit

---
+++ bot.js
@@ -0,0 +1,24 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Bot is ready!');
+});
+
+client.on('message', message => {
+    console.log("Message received: " + message.body);
+
+    if(message.body.toLowerCase() === "hi"){
+        message.reply("Hello 👋 How can I help you today?");
+    }
+});
+
+client.initialize();
