e3103bb68e5fd44b32e934bcb7a5bb399356b470
Nehemiah Jerald <nehemiahjerald@Nehemiahs-MacBook-Pro.local>
2026-02-22 18:08:34 +0500

    added no sandbox fix

--- bot.js
+++ bot.js
@@ -2,7 +2,11 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
 
 client.on('qr', qr => {
