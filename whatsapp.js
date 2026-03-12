99382cb35bfc5850c8bba09676817103711a68a1
Nehemiah Jerald <nehemiahjerald@Nehemiahs-MacBook-Pro.local>
2026-03-12 17:49:32 +0500

    fixed whatsapp reply sending

--- whatsapp.js
+++ whatsapp.js
@@ -1,22 +1,55 @@
 import axios from "axios"
+import { handleConversation } from "./services/conversationService.js"
 
-const token = process.env.WHATSAPP_TOKEN
-const phoneId = process.env.PHONE_NUMBER_ID
+const TOKEN = process.env.WHATSAPP_TOKEN
+const PHONE_ID = process.env.PHONE_NUMBER_ID
 
-export async function sendMessage(to, text) {
 
-  await axios.post(
-    `https://graph.facebook.com/v19.0/${phoneId}/messages`,
-    {
-      messaging_product: "whatsapp",
-      to: to,
-      text: { body: text }
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json"
-      }
+export async function handleWebhook(req, res) {
+
+    const body = req.body
+
+    if (body.object) {
+
+        const entry = body.entry?.[0]
+        const change = entry?.changes?.[0]
+        const value = change?.value
+        const messages = value?.messages
+
+        if (messages) {
+
+            const message = messages[0]
+            const phone = message.from
+            const text = message.text?.body
+
+            console.log("Incoming:", text)
+
+            const reply = await handleConversation(phone, text)
+
+            if (reply) {
+
+                await axios.post(
+                    `https://graph.facebook.com/v19.0/${PHONE_ID}/messages`,
+                    {
+                        messaging_product: "whatsapp",
+                        to: phone,
+                        text: { body: reply }
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${TOKEN}`,
+                            "Content-Type": "application/json"
+                        }
+                    }
+                )
+            }
+        }
+
+        res.sendStatus(200)
+
+    } else {
+
+        res.sendStatus(404)
+
     }
-  )
 }
