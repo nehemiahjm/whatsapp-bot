ba05bff23be53937a3ea9286a0094cbc9d3ea1b0
Nehemiah Jerald <nehemiahjerald@Nehemiahs-MacBook-Pro.local>
2026-03-12 17:42:53 +0500

    fixed userService exports

--- services/userService.js
+++ services/userService.js
@@ -1,21 +1,42 @@
 import pool from "../database/postgres.js"
 
-export async function getUserByPhone(phone) {
 
-  const result = await pool.query(
-    "SELECT * FROM users WHERE phone=$1",
-    [phone]
-  )
+export async function getUser(phone) {
 
-  return result.rows[0]
+    const result = await pool.query(
+        "SELECT * FROM users WHERE phone = $1",
+        [phone]
+    )
 
+    return result.rows[0]
 }
+
+
 
 export async function createUser(phone) {
 
-  await pool.query(
-    "INSERT INTO users (phone) VALUES ($1)",
-    [phone]
-  )
+    await pool.query(
+        "INSERT INTO users (phone, state) VALUES ($1, 'new_user')",
+        [phone]
+    )
+}
 
+
+
+export async function updateUserLanguage(phone, language) {
+
+    await pool.query(
+        "UPDATE users SET language = $1 WHERE phone = $2",
+        [language, phone]
+    )
+}
+
+
+
+export async function updateUserState(phone, state) {
+
+    await pool.query(
+        "UPDATE users SET state = $1 WHERE phone = $2",
+        [state, phone]
+    )
 }
